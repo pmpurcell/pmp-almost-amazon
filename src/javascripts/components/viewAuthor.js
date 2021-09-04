0d618e1aa318a36cbcb64b316b768b394283270d
pmpurcell93 <maddenpurcell@gmail.com>
2021-09-03 22:32:18 -0500

    Added quote button to author details page

--- src/javascripts/components/viewAuthor.js
+++ src/javascripts/components/viewAuthor.js
@@ -10,9 +10,10 @@ const viewAuthor = (obj) => {
        <div class="mt-5">
          <i id="edit-book-btn--${obj.firebaseKey}" class="fas fa-edit btn btn-info"></i>
          <i id="delete-book--${obj.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+         <i id="add-quote--${obj.firebaseKey}" class="btn btn-dark fas fa-comment-alt"></i>
        </div>
      </div>
-     <div id= "quotes"></div>
+     <div id="quotes"></div>
      <div class="text-white ms-5 details">
        <h5>${obj.first_name} ${obj.last_name}</h5>
        <p>${obj.email || ''}</p>
